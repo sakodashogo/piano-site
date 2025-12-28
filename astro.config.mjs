39ad31cfb35c44fff3416d043aa7b5a7742f659f
sakodashogo <sakodasakodasakoda5555@gmail.com>
2025-12-29 03:07:30 +0900

    add sitemap integrationgit push

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://piano-site.pages.dev', // ← ここを追加（自分のURL）
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
